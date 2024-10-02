32400ad6816a2253fd09320640ea60fe80346e56
simon.montt <simon.montt@gmail.com>
2024-10-02 18:45:20 -0300

    Fixing deployment in streamlit

--- frontend_plotly_upd.py
+++ frontend_plotly_upd.py
@@ -117,7 +117,7 @@ features['station_encoded'] = station_label_encoder.fit_transform(features['stat
 predictions['line'] = line_label_encoder.inverse_transform(predictions['line'])
 predictions['station'] = station_label_encoder.inverse_transform(predictions['station'])
 
-print(predictions)
+print(predictions, predictions.shape)
 
 # Get unique lines for dropdowns
 unique_lines = sorted(features['line'].unique())
