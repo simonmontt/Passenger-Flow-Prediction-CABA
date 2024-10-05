09d43297132a6d2a31eeed591e2794e47e948af2
simon.montt <simon.montt@gmail.com>
2024-10-05 19:35:16 -0300

    Fix error for deployment

--- frontend_plotly_upd.py
+++ frontend_plotly_upd.py
@@ -29,8 +29,8 @@ def cached_load_batch_of_features() -> pd.DataFrame:
 @st.cache_data
 def cached_load_predictions() -> pd.DataFrame:
     current_time = datetime.now().replace(minute=0, second=0, microsecond=0)
-    #from_hour_of_entry = from_hour_of_entry - timedelta(hours=1) 
-    to_hour_of_entry = current_time - timedelta(hours=2)
+    #from_hour_of_entry = current_time  
+    to_hour_of_entry = current_time -timedelta(hours=2)
     return load_predictions_from_store(to_hour_of_entry)
 
 # Function for loading historical data for comparison (previous year's data)
