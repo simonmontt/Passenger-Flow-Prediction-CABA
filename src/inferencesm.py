3e323b2726ad03851529c078067410b19d647899
simon.montt <simon.montt@gmail.com>
2024-10-05 10:25:58 -0300

    Fix error for deployment

--- src/inferencesm.py
+++ src/inferencesm.py
@@ -149,7 +149,7 @@ def load_predictions_from_store(from_hour_of_entry: datetime, to_hour_of_entry: 
     print(f'Fetching predictions between {from_hour_of_entry} and {to_hour_of_entry}')
     predictions = predictions_fv.get_batch_data(
         start_time=from_hour_of_entry - timedelta(days=1),
-        end_time=to_hour_of_entry + timedelta(days=1)
+        end_time=to_hour_of_entry + timedelta(days=1) + timedelta(hours=3)
     )
 
     # Ensure datetimes are UTC-aware
